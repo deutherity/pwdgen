8679471c5a1a49f1e623326883aa30058326c63b
aethernet <deutherity@gmail.com>
2021-06-15 21:20:49 +0300

    fixed bug with additional chars for hash

--- genpw.cpp
+++ genpw.cpp
@@ -19,7 +19,7 @@ std::string genpw(const std::string_view &data, std::uint16_t retlen,
     genhash((uchar *)data.data(), data.length(), hash, retlen * 2, salt,
             saltlen);
     size_t i = 0;
-    size_t charset2howmany = std::min(addalphabet.length(), (std::size_t) retlen % 5);
+    size_t charset2howmany = std::min(addalphabet.length(), (std::size_t) retlen / 5);
     for (; i < retlen - charset2howmany; ++i) {
         shuffleVec.push_back(alnums[hash[i] % alnums.length()]);
     }
